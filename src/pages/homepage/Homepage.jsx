c170e491e82ce63d59d937058cfe3da9d6ffd025
Adetoba Adedeji <adetoba22@gmail.com>
2021-12-16 15:06:46 +0100

    removed zx7 section

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import HeroSection from '../../components/heroSection/HeroSection'
 import CategorySection from '../../components/categorySection/CategorySection'
 import ZX9SpeakerSection from '../../components/ZX9SpeakerSection/ZX9SpeakerSection'
-import ZX7SpeakerSection from '../../components/ZX7SpeakerSection/ZX7SpeakerSection'
+// import ZX7SpeakerSection from '../../components/ZX7SpeakerSection/ZX7SpeakerSection'
 import XY1EarphonesSection from '../../components/XY1EarphonesSection/XY1EarphonesSection'
 import AboutSection from '../../components/aboutSection/AboutSection'
 
@@ -12,7 +12,7 @@ const Homepage = () => {
 			<HeroSection />
 			<CategorySection />
 			<ZX9SpeakerSection />
-			<ZX7SpeakerSection />
+			{/* <ZX7SpeakerSection /> */}
 			<XY1EarphonesSection />
 			<AboutSection />
 		</div>
